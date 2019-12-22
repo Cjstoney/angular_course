36227d41337257a3a316a3b8665bebca6292f411
Camerone Stoney <Cameronestoney@yahoo.com>
2019-12-22 10:06:12 -0800

    installed bootstrap dependency and added it to the styles portion of the angular.json file.  Deleted all code from the app.component.html file to start from scratch and removed the module from the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule}from '@angular/forms';
-import { AppComponent } from './app.component';
+ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -9,7 +8,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
