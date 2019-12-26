c2fb89d3d1720b8538a50e2969108483f5113dff
Camerone Stoney <Cameronestoney@yahoo.com>
2019-12-26 10:14:28 -0800

    a lot changed in order to get the successful component to work.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningComponent } from './warning/warning.component';
+import {SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningComponent
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
