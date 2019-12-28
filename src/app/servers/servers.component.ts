87bc0fd52bcb186ef836a5ff7d046b2afd18d123
Camerone Stoney <Cameronestoney@yahoo.com>
2019-12-28 09:08:39 -0800

    starting learnig string interpilation and data binding with basic examples in the servers and server component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServers: boolean = false;
+  constructor() { 
+    setTimeout(()=>{ 
+      this.allowNewServers = true
+    },2000)
+  }
 
   ngOnInit() {
   }
